5b23542c6f020ab2afdaebc9ce6148576076c778
Shubham Baisware <shubhambaisware1@gmail.com>
2021-10-16 17:49:55 +0530

    method twoNumbersCommaSeperatedReturnsSum() added and tested failed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,10 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        if ( input.isEmpty()) {
+        if ( isEmpty(input)) {
         	return 0;
         }
         return stringToInt(input);
+    }
+    
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
     }
     
     private int stringToInt(String input) {
